3db653869f9c4f771bd25d3746f90d5a283beb65
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-06-03 18:11:27 -0700

    feat: photo album

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import BookApp from './interviewPrepTasks/ReactQuestions/BookApp';
 import { FormWithStateManagement } from './interviewPrepTasks/ReactQuestions/FormWithStateManagement';
 import PostsComponent from './interviewPrepTasks/ReactQuestions/PostsComponent';
 import { Layouts } from './interviewPrepTasks/ReactQuestions/Layouts';
+import { AlbumPhotoApplication } from './interviewPrepTasks/ReactQuestions/AlbumPhotoApplication';
 
 function App() {
   return (
     <div className='App'>
+      <AlbumPhotoApplication />
       <Layouts />
       <JsonViewer />
       <BookApp />
