c6d9b44fe5653724791f4a4da51a53987b8b9dcf
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-08-08 17:16:17 -0700

    my promise implementation

---
+++ src/interviewPrepTasks/ReactQuestions/CustomPromises/MyPromise.js
@@ -0,0 +1,166 @@
+const STATE = {
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected',
+  PENDING: 'pending',
+};
+
+class MyPromise {
+  #thenCbs = [];
+  #catchCbs = [];
+  #state = STATE.PENDING;
+  #value;
+  #onSuccessBind = this.#onSuccess.bind(this);
+  #onFailBind = this.#onFail.bind(this);
+
+  constructor(cb) {
+    try {
+      cb(this.#onSuccessBind, this.#onFailBind);
+    } catch (e) {
+      this.#onFail(e);
+    }
+  }
+
+  #runCallbacks() {
+    if (this.#state === STATE.FULFILLED) {
+      this.#thenCbs.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#thenCbs = [];
+    }
+
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCbs.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#catchCbs = [];
+    }
+  }
+
+  #onSuccess(value) {
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof MyPromise) {
+        value.then(this.#onSuccessBind, this.#onFailBind);
+        return;
+      }
+
+      this.#value = value;
+      this.#state = STATE.FULFILLED;
+      this.#runCallbacks();
+    });
+  }
+
+  #onFail(value) {
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof MyPromise) {
+        value.then(this.#onSuccessBind, this.#onFailBind);
+        return;
+      }
+
+      if (this.#catchCbs.length === 0) {
+        throw new UncaughtPromiseError(value);
+      }
+
+      this.#value = value;
+      this.#state = STATE.REJECTED;
+      this.#runCallbacks();
+    });
+  }
+
+  then(thenCb, catchCb) {
+    return new MyPromise((resolve, reject) => {
+      this.#thenCbs.push((result) => {
+        if (thenCb == null) {
+          resolve(result);
+          return;
+        }
+        try {
+          resolve(thenCb(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.#catchCbs.push((result) => {
+        if (catchCb == null) {
+          resolve(result);
+          return;
+        }
+
+        try {
+          resolve(catchCb(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.#runCallbacks();
+    });
+  }
+
+  catch(cb) {
+    return this.then(undefined, cb);
+  }
+  finally(cb) {
+    return this.then(
+      (result) => {
+        cb();
+        return result;
+      },
+      (result) => {
+        cb();
+        throw result;
+      }
+    );
+  }
+
+  static resolve(value) {
+    return new MyPromise((resolve) => {
+      resolve(value);
+    });
+  }
+
+  static reject(value) {
+    return new MyPromise((resolve, reject) => {
+      reject(value);
+    });
+  }
+
+  static all(promises) {
+    return new MyPromise((resolve, reject) => {
+      if (!Array.isArray(promises)) {
+        return reject(new TypeError('Argument is not iterable'));
+      }
+
+      const resolvedValues = Array(promises.length);
+      let resolvedCount = 0;
+
+      if (promises.length === 0) return resolve(resolvedValues);
+
+      promises.forEach((promise, index) => {
+        MyPromise.resolve(promise)
+          .then((value) => {
+            resolvedValues[index] = value;
+            resolvedCount += 1;
+
+            if (resolvedCount === promise.length) {
+              resolve(resolvedValues);
+            }
+          })
+          .catch(reject);
+      });
+    });
+  }
+}
+
+class UncaughtPromiseError extends Error {
+  constructor(error) {
+    super(error);
+    this.stack = `(in promise) ${error}`;
+  }
+}
+
+module.exports = MyPromise;
