0d0c400155a5fb409df9d150e63c558b356d3682
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-08-11 16:34:58 -0700

    fix custom reducer

--- src/interviewPrepTasks/AlgoQuestions/CustomReducer.js
+++ src/interviewPrepTasks/AlgoQuestions/CustomReducer.js
@@ -4,7 +4,25 @@
  * @param {U} [initialValue]
  * @return {U}
  */
-Array.prototype.myReduce = function (callbackFn, initialValue) {};
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  if (typeof callbackFn !== 'function') throw TypeError('Not a function');
+
+  let accumulator = initialValue;
+  let index = 0;
+
+  if (typeof initialValue === 'undefined') {
+    accumulator = this[0];
+    index = 1;
+  }
+
+  for (let i = index; i < this.length; i++) {
+    if (i in this) {
+      accumulator = callbackFn(accumulator, this[i], i, this);
+    }
+  }
+
+  return accumulator;
+};
 
 console.log([1, 2, 3].myReduce((prev, curr) => prev + curr, 0)); // 6
 console.log([1, 2, 3].myReduce((prev, curr) => prev + curr, 4)); // 10
