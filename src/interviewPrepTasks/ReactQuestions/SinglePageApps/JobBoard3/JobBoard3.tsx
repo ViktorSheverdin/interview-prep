5a9d48ae7b963909529e779e7a58061425ed122b
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-09-09 18:19:54 -0700

    chore: job board 3

---
+++ src/interviewPrepTasks/ReactQuestions/SinglePageApps/JobBoard3/JobBoard3.tsx
@@ -0,0 +1,11 @@
+const JOB_IDS_URL = 'https://hacker-news.firebaseio.com/v0/jobstories.json';
+const JOB_DETAILS_URL = 'https://hacker-news.firebaseio.com/v0/item';
+// const res = await fetch(`${JOB_DETAILS_URL}/${id}.json`);
+
+export const JobBoard3 = () => {
+  return (
+    <div>
+      <div>Job Board 3</div>
+    </div>
+  );
+};
