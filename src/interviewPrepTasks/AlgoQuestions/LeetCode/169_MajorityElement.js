f1dcb69a8fea6463b8673b723946401863ddeaa7
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-09-17 18:19:41 -0700

    chore: practice

--- src/interviewPrepTasks/AlgoQuestions/LeetCode/169_MajorityElement.js
+++ src/interviewPrepTasks/AlgoQuestions/LeetCode/169_MajorityElement.js
@@ -1,21 +1,33 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+// Constraints:
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var majorityElement = function (nums) {
-  let canditate = nums[0];
+  let candidate = nums[0];
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
     if (count === 0) {
-      canditate = nums[i];
+      candidate = nums[0];
     }
-    if (canditate === nums[i]) {
+    if (nums[i] === candidate) {
       count++;
     } else {
       count--;
     }
   }
-  return canditate;
+  return candidate;
 };
 
 console.log(majorityElement([3, 2, 3])); // 3
