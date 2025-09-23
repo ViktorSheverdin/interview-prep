e20cde7d6e90a4c4cee7ea760c22bbb2b0817eed
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-09-22 17:20:51 -0700

    feat: add graqhql

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { createHandler } from 'graphql-http/lib/use/express';
+import express from 'express';
+
+// Construct a schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'Hello world!',
+      },
+    },
+  }),
+});
+
+const app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+  })
+);
+
+// Start the server at port
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
