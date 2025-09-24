5d938110431ac8c1f16cf08adab240333afc5dde
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2025-09-23 18:56:11 -0700

    chore: first queries

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,94 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} from 'graphql';
 import { createHandler } from 'graphql-http/lib/use/express';
 import express from 'express';
 
-// Construct a schema
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => 'Hello world!',
+const authors = [
+  { id: 1, name: 'J. K. Rowling' },
+  { id: 2, name: 'J. R. R. Tolkien' },
+  { id: 3, name: 'Brent Weeks' },
+];
+
+const books = [
+  { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+  { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+  { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+  { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+  { id: 5, name: 'The Two Towers', authorId: 2 },
+  { id: 6, name: 'The Return of the King', authorId: 2 },
+  { id: 7, name: 'The Way of Shadows', authorId: 3 },
+  { id: 8, name: 'Beyond the Shadows', authorId: 3 },
+];
+
+const AuthorType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'This represents an author with books',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
+    // books: {
+    //   type: new GraphQLList(BookType),
+    //   resolve: (author) => {
+    //     return books.find((book) => book.authorId === author.id);
+    //   },
+    // },
+  }),
+});
+
+const BookType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLString) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
       },
     },
   }),
 });
 
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of books',
+      resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'List of authors',
+      resolve: () => authors,
+    },
+  }),
+});
+
+// Construct a schema
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
 const app = express();
 
 // Create and use the GraphQL handler.
-app.all(
+app.use(
   '/graphql',
   createHandler({
     schema: schema,
@@ -28,3 +98,15 @@ app.all(
 // Start the server at port
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// CURL
+// curl -X POST \
+//   -H "Content-Type: application/json" \
+//   --data '{"query":"{ books {name author {name}} }"}' \
+//   http://localhost:4000/graphql
+
+// CURL
+// curl -X POST \
+//   -H "Content-Type: application/json" \
+//   --data '{"query":"{ authors {name books {name}} }"}' \
+//   http://localhost:4000/graphql
---
+++ frontend/src/interviewPrepTasks/AlgoQuestions/LeetCode/424_LongestRepeatingCharacterReplacement.js
@@ -0,0 +1,17 @@
+// You are given a string s and an integer k.
+// You can choose any character of the string and change it
+// to any other uppercase English character.
+// You can perform this operation at most k times.
+
+// Return the length of the longest substring
+// containing the same letter you can get after performing the above operations.
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {};
+
+console.log(characterReplacement('ABAB', 2)); // 4
+console.log(characterReplacement('AABABBA', 1)); // 4
